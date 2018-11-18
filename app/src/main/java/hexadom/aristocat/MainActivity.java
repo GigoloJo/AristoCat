dfd028fb5ae50a0c4fa56cbb88e9ee4e72db4ef8
GigoloJo <GigoloJo@users.noreply.github.com>
2018-11-18 17:24:19 +0100

    - changed Aristeia image buttons to proper text buttons - fixed bug in main activity

--- app/src/main/java/hexadom/aristocat/MainActivity.java
+++ app/src/main/java/hexadom/aristocat/MainActivity.java
@@ -3,8 +3,6 @@ package hexadom.aristocat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Html;
-import android.text.Spanned;
 import android.view.View;
 
 import uk.co.deanwild.flowtextview.FlowTextView;
@@ -19,6 +17,21 @@ public class MainActivity extends AppCompatActivity {
 
     public void goto_characters(View v){
         Intent myIntent = new Intent(getBaseContext(), CharactersActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void goto_scenarios(View v){
+        Intent myIntent = new Intent(getBaseContext(), ScenariosActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void goto_states(View v){
+        Intent myIntent = new Intent(getBaseContext(), StatesActivity.class);
+        startActivity(myIntent);
+    }
+
+    public void goto_round(View v){
+        Intent myIntent = new Intent(getBaseContext(), RoundActivity.class);
         startActivity(myIntent);
     }
 
